4d5b291a16fbf295d071528b28784f9699def73f
abissem <e95bso@live.fr>
2020-12-27 23:49:36 +0100

    Add ROOM + Dao database + Clear Architecture

--- app/src/main/java/com/example/android4a/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/android4a/presentation/main/MainActivity.kt
@@ -1,22 +1,22 @@
-package com.example.android4a
+package com.example.android4a.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import com.example.android4a.R
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val mainViewModel: MainViewModel by inject()
+    private val mainViewModel: MainViewModel by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         main_button.setOnClickListener {
-            mainViewModel.onClickedIncrement()
+            mainViewModel.onClickedIncrement("")
         }
         mainViewModel.counter.observe(this, Observer {
             value -> main_text.text = value.toString()
